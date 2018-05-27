43aa3c26f6b70bf9eeb14e608f83d35983956b83
Filip Kudła <filip.kudla@snow.dog>
2018-05-27 09:25:26 +0200

    Add changes in config add autogenerated models and seeders

--- models/index.js
+++ models/index.js
@@ -1,25 +1,26 @@
 'use strict';
 
-const fs        = require('fs'),
-      path      = require('path'),
-      Sequelize = require('sequelize'),
-      basename  = path.basename(__filename),
-      db        = {};
+var fs        = require('fs');
+var path      = require('path');
+var Sequelize = require('sequelize');
+var basename  = path.basename(__filename);
+var env       = process.env.NODE_ENV || 'development';
+var config    = require(__dirname + '/../config/config.js')[env];
+var db        = {};
 
-const sequelize = new Sequelize(CONFIG.db_name, CONFIG.db_user, CONFIG.db_password, {
-  host: CONFIG.db_host,
-  dialect: CONFIG.db_dialect,
-  port: CONFIG.db_port,
-  operatorsAliases: false
-});
+if (config.use_env_variable) {
+  var sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  var sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
 
-fs.readdirSync(__dirname)
+fs
+  .readdirSync(__dirname)
   .filter(file => {
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
   .forEach(file => {
-    const model = sequelize['import'](path.join(__dirname, file));
-
+    var model = sequelize['import'](path.join(__dirname, file));
     db[model.name] = model;
   });
 
